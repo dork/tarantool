dork/tarantool

commit 6965dc8a4a78bd4927d5fe4f9d896077ae5d2d32

BitmapIndex refactoring
* Replace bitmap_iterator_newXXX with a new expression API (expr.h)
* Reduce the overall number of mallocs in bitmap_iterator (via new API)
* Split "new", "init" and "rewind" functions in iterators
* Allow reusage of existing iterators
* Update doxygen-style comments

src/box/bitmap/expr.c
@@ -1,0 +1,234 @@
+/*
+ * Redistribution and use in source and binary forms, with or
+ * without modification, are permitted provided that the following
+ * conditions are met:
+ *
+ * 1. Redistributions of source code must retain the above
+ *    copyright notice, this list of conditions and the
+ *    following disclaimer.
+ *
+ * 2. Redistributions in binary form must reproduce the above
+ *    copyright notice, this list of conditions and the following
+ *    disclaimer in the documentation and/or other materials
+ *    provided with the distribution.
+ *
+ * THIS SOFTWARE IS PROVIDED BY <COPYRIGHT HOLDER> ``AS IS'' AND
+ * ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED
+ * TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR
+ * A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL
+ * <COPYRIGHT HOLDER> OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT,
+ * INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
+ * DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
+ * SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR
+ * BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF
+ * LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+ * (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF
+ * THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF
+ * SUCH DAMAGE.
+ */
+
+#include "expr.h"
+#include "expr_p.h"
+
+#include <malloc.h>
+
+const size_t EXPR_DEFAULT_CAPACITY = 2;
+const size_t EXPR_GROUP_DEFAULT_CAPACITY = 32;
+
+struct bitmap_expr *
+bitmap_expr_new()
+{
+	struct bitmap_expr *expr = calloc(1, sizeof(*expr));
+	if (expr == NULL) {
+		goto error_0;
+	}
+
+	expr->groups = calloc(EXPR_DEFAULT_CAPACITY, sizeof(*expr->groups));
+	if (expr->groups == NULL) {
+		goto error_1;
+	}
+
+	expr->groups_capacity = EXPR_DEFAULT_CAPACITY;
+
+	for (size_t g = 0; g < EXPR_DEFAULT_CAPACITY; g++) {
+		struct bitmap_expr_group *group;
+		/* allocate memory for structure with flexible array */
+		size_t size = sizeof(*group) +
+			sizeof(*(group->elements)) * EXPR_GROUP_DEFAULT_CAPACITY;
+		group = malloc(size);
+		if (group == NULL) {
+			goto error_2;
+		}
+
+		group->elements_capacity = EXPR_GROUP_DEFAULT_CAPACITY;
+		group->post_op = BITMAP_OP_NULL;
+		group->reduce_op = BITMAP_OP_OR;
+		expr->groups[g] = group;
+	}
+
+	bitmap_expr_clear(expr);
+
+	return expr;
+error_2:
+	for (size_t g = 0; g < expr->groups_capacity; g++) {
+		if (expr->groups[g] != NULL) {
+			free(expr->groups[g]);
+		}
+	}
+	free(expr->groups);
+error_1:
+	free(expr);
+error_0:
+	return NULL;
+}
+
+void
+bitmap_expr_free(struct bitmap_expr *expr)
+{
+	if (expr == NULL) {
+		return;
+	}
+
+	for (size_t g = 0; g < expr->groups_capacity; g++) {
+		free(expr->groups[g]);
+	}
+
+	free(expr->groups);
+	free(expr);
+}
+
+void
+bitmap_expr_clear(struct bitmap_expr *expr)
+{
+	assert(expr != NULL);
+	for (size_t g = 0; g < expr->groups_capacity; g++) {
+		expr->groups[g]->elements_size = 0;
+	}
+
+	expr->groups_size = 0;
+}
+
+static int
+expr_reserve(struct bitmap_expr *expr, size_t capacity)
+{
+	if (expr->groups_capacity >= capacity) {
+		return 0;
+	}
+
+	struct bitmap_expr_group **groups =
+		realloc(expr->groups, capacity * sizeof(*expr->groups));
+
+	if (groups == NULL) {
+		return 0;
+	}
+
+	expr->groups = groups;
+	for (size_t g = expr->groups_capacity; g < capacity; g++) {
+		struct bitmap_expr_group *group;
+		size_t size = sizeof(*group) +
+			sizeof(*(group->elements)) * EXPR_GROUP_DEFAULT_CAPACITY;
+		group = calloc(1, size);
+		if (group == NULL) {
+			return -1;
+		}
+		expr->groups[g] = group;
+		expr->groups_capacity++;
+	}
+
+	return 0;
+}
+
+static int
+expr_reserve_group(struct bitmap_expr *expr, size_t group_id, size_t capacity)
+{
+	assert(expr->groups_capacity > group_id);
+
+	struct bitmap_expr_group *orig_group = expr->groups[group_id];
+
+	if (orig_group->elements_capacity >= capacity) {
+		return 0;
+	}
+
+	size_t capacity2 = orig_group->elements_capacity;
+	while (capacity2 < capacity) {
+		capacity2 *= 2;
+	}
+
+	struct bitmap_expr_group *new_group;
+	size_t size = sizeof(*new_group) +
+		sizeof(*new_group->elements) * capacity2;
+	new_group = calloc(1, size);
+	if (new_group == NULL) {
+		return -1;
+	}
+
+	memcpy(new_group,
+	       orig_group,
+	       sizeof(*orig_group) +
+	       sizeof(*orig_group->elements) * orig_group->elements_capacity);
+
+	new_group->elements_capacity = capacity2;
+	expr->groups[group_id] = new_group;
+
+	free(orig_group);
+
+	return 0;
+}
+
+int
+bitmap_expr_add_group(struct bitmap_expr *expr,
+		      enum bitmap_binary_op reduce_op,
+		      enum bitmap_unary_op post_op)
+{
+	assert(expr != NULL);
+
+	const size_t group_id = expr->groups_size;
+	if (expr_reserve(expr, group_id + 1) != 0) {
+		return -1;
+	}
+
+	expr->groups[group_id]->elements_size = 0;
+	expr->groups[group_id]->reduce_op = reduce_op;
+	expr->groups[group_id]->post_op = post_op;
+	expr->groups_size++;
+
+	return 0;
+}
+
+size_t
+bitmap_expr_size(struct bitmap_expr *expr)
+{
+	return expr->groups_size;
+}
+
+int
+bitmap_expr_group_add_bitmap(struct bitmap_expr *expr,
+			     size_t group_id,
+			     struct bitmap *bitmap,
+			     enum bitmap_unary_op pre_op)
+{
+	assert(expr != NULL);
+	assert(expr->groups_size > group_id);
+	assert(bitmap != NULL);
+
+	const size_t bitmap_id = expr->groups[group_id]->elements_size;
+	if (expr_reserve_group(expr, group_id, bitmap_id + 1) != 0) {
+		return -1;
+	}
+
+	expr->groups[group_id]->elements[bitmap_id].bitmap = bitmap;
+	expr->groups[group_id]->elements[bitmap_id].pre_op = pre_op;
+	expr->groups[group_id]->elements_size++;
+
+	return 0;
+}
+
+size_t
+bitmap_expr_group_size(struct bitmap_expr *expr,
+		       size_t group_id)
+{
+	assert(expr != NULL);
+	assert(expr->groups_size > group_id);
+
+	return expr->groups[group_id]->elements_size;
+}

src/box/bitmap/expr.h
@@ -1,0 +1,159 @@
+#ifndef BITMAP_EXPR_H_INCLUDED
+#define BITMAP_EXPR_H_INCLUDED
+
+/*
+ * Redistribution and use in source and binary forms, with or
+ * without modification, are permitted provided that the following
+ * conditions are met:
+ *
+ * 1. Redistributions of source code must retain the above
+ *    copyright notice, this list of conditions and the
+ *    following disclaimer.
+ *
+ * 2. Redistributions in binary form must reproduce the above
+ *    copyright notice, this list of conditions and the following
+ *    disclaimer in the documentation and/or other materials
+ *    provided with the distribution.
+ *
+ * THIS SOFTWARE IS PROVIDED BY <COPYRIGHT HOLDER> ``AS IS'' AND
+ * ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED
+ * TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR
+ * A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL
+ * <COPYRIGHT HOLDER> OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT,
+ * INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
+ * DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
+ * SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR
+ * BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF
+ * LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+ * (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF
+ * THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF
+ * SUCH DAMAGE.
+ */
+
+/**
+ * @file
+ * @brief Expressions for bitmap iterator.
+ *
+ * Iterator can apply logical operations on bitmaps on the fly, without
+ * producing temporary bitmaps. At first stage, bitmap.pre_op is applied to
+ * each bitmap of the group. Then reduce_op is cumulatively applied to the items
+ * from left to right (like foldl), so as to reduce the group to a single value.
+ * Example: group1 = b1 || ~b2 || b3 || ~b4. After that group.post_op is applied
+ * on the result after that. On next step, iterator combines results from all
+ * groups using binary AND operation.
+ * Example: (group1) & ~(group2) & ~(group3)
+ * Due to fact that every propositional formula can be represented using CNF,
+ * you can construct any logical expression you want using scheme above.
+ *
+ * @link http://en.wikipedia.org/wiki/Conjunctive_normal_form @endlink
+ * @note Reduce operations in both cases are left-associate.
+ *
+ * @see bitmap_iterator_set_expr
+ * @author Roman Tsisyk
+ */
+
+#include "bitmap.h"
+
+/**
+ * @brief  Unary operation
+ * @see bitmap_expr
+ */
+enum bitmap_unary_op {
+	/** return bitmaps as is, without applying any unary operation */
+	BITMAP_OP_NULL = 0,
+	/** inverse (apply bitwise NOT) bits in the bitmap  */
+	BITMAP_OP_NOT  = 0x1
+};
+
+/**
+ * @brief Binary operation type
+ * @see bitmap_expr
+ */
+enum bitmap_binary_op {
+	/** bitwise AND */
+	BITMAP_OP_AND   = 0x1 << 8,
+	/** bitwise NOT AND */
+	BITMAP_OP_NAND  = 0x2 << 8,
+	/** bitwise OR */
+	BITMAP_OP_OR    = 0x3 << 8,
+	/** bitwise NOT OR */
+	BITMAP_OP_NOR   = 0x4 << 8,
+	/** bitwise XOR */
+	BITMAP_OP_XOR   = 0x5 << 8,
+	/** bitwise NOT XOR */
+	BITMAP_OP_XNOR  = 0x6 << 8
+};
+
+struct bitmap_expr;
+
+/**
+ * @brief Create new bitmap expression
+ * @return Pointer to expression object or NULL if memory allocation has failed
+ */
+struct bitmap_expr *
+bitmap_expr_new();
+
+/**
+ * @brief Destroy @expr object
+ * @param expr
+ */
+void
+bitmap_expr_free(struct bitmap_expr *expr);
+
+
+/**
+ * @brief Clear @a expression (remove all groups)
+ * @param expr object
+ */
+void
+bitmap_expr_clear(struct bitmap_expr *expr);
+
+
+/**
+ * @brief Add a new group to @a expression.
+ * Resulting group is look like (post_op (b1 reduce_op ~b2 reduce_op b3)).
+ * @param expr object
+ * @param reduce_op operation applied to pairs of bitmaps in the expr
+ * @param post_op operation applied to result
+ * @return zero on success and non-zero otherwise
+ * @see bitmap_iterator_set_expr()
+ */
+int
+bitmap_expr_add_group(struct bitmap_expr *expr,
+		      enum bitmap_binary_op reduce_op,
+		      enum bitmap_unary_op post_op);
+
+/**
+ * @brief Returns number of groups in the @a expr
+ * @param expr object
+ * @return number of groups in the @a expr
+ */
+size_t
+bitmap_expr_size(struct bitmap_expr *expr);
+
+
+/**
+ * @brief Adds @a bitmap to group with id @a group_id
+ * @param expr object
+ * @param group_id group identifier
+ * @param bitmap bitmap
+ * @param pre_op operation that applied to bitmap
+ * @return zero on success and non-zero otherwise
+ */
+int
+bitmap_expr_group_add_bitmap(struct bitmap_expr *expr,
+			     size_t group_id,
+			     struct bitmap *bitmap,
+			     enum bitmap_unary_op pre_op);
+
+/**
+ * @brief Returns number of bitmaps in the @a group
+ * @param expr object
+ * @param group_id group identifier
+ * @return Number of bitmaps in the @a group
+ */
+size_t
+bitmap_expr_group_size(struct bitmap_expr *expr,
+		       size_t group_id);
+
+#endif /* BITMAP_EXPR_H_INCLUDED */

src/box/bitmap/index.c
@@ -164,161 +164,97 @@ int bitmap_index_remove(struct bitmap_index *index,
 	return 0;
 }
 
-static
-int bitmap_index_iterate_equals(struct bitmap_index *index,
-			       struct bitmap_iterator **pit,
-			       void *key, size_t key_size)
+int bitmap_index_iterate_equals(
+			struct bitmap_index *index,
+			struct bitmap_expr *expr,
+			void *key, size_t key_size)
 {
-	int rc = 0;
+	assert (index != NULL);
+	assert (expr != NULL);
 
-	size_t key_bits = (key_size * CHAR_BIT);
-	size_t elements_size = 1;
+	bitmap_expr_clear(expr);
 
-	for (size_t pos = 0; pos < key_bits; pos++) {
-		size_t b = pos + 1;
-		if (test_bit(key, pos)) {
-			if (index->bitmaps[b] == NULL ||
-				index->bitmaps_size <= pos) {
-				elements_size = 0;
-				break;
-			}
-
-			elements_size++;
-		} else {
-			if (index->bitmaps[b] == NULL
-				|| index->bitmaps_size <= pos) {
-				continue;
-			}
-			elements_size++;
-		}
-	}
-
-	struct bitmap_iterator_group *set;
-	size_t set_size = sizeof(*set) + sizeof(*(set->elements)) *
-			elements_size;
-	set = calloc(1, set_size);
-	if (!set) {
+	if (bitmap_expr_add_group(expr, BITMAP_OP_AND, BITMAP_OP_NULL) != 0) {
 		return -1;
 	}
-
-	set->reduce_op = BITMAP_OP_AND;
-	set->post_op = BITMAP_OP_NULL;
-	set->elements_size = 0;
-
-	if (elements_size == 0) {
-		goto exit;
-	}
-
-	set->elements[set->elements_size].bitmap = index->bitmaps[0];
-	set->elements[set->elements_size].pre_op = BITMAP_OP_NULL;
-	set->elements_size++;
 
 	for (size_t pos = 0; pos < key_size * CHAR_BIT; pos++) {
 		size_t b = pos + 1;
 		if (test_bit(key, pos)) {
-			assert (index->bitmaps[b] != NULL);
+			if (index->bitmaps[b] == NULL) {
+				bitmap_expr_clear(expr);
+				break;
+			}
 
-			set->elements[set->elements_size].bitmap =
-					index->bitmaps[b];
-			set->elements[set->elements_size].pre_op =
-					BITMAP_OP_NULL;
-			set->elements_size++;
+			if (bitmap_expr_group_add_bitmap(expr, 0,
+				index->bitmaps[b], BITMAP_OP_NULL) != 0) {
+				return -1;
+			}
 		} else {
 			if (index->bitmaps[b] == NULL) {
 				continue;
 			}
 
-			set->elements[set->elements_size].bitmap =
-					index->bitmaps[b];
-			set->elements[set->elements_size].pre_op =
-					BITMAP_OP_NOT;
-			set->elements_size++;
+			if (bitmap_expr_group_add_bitmap(expr, 0,
+				index->bitmaps[b], BITMAP_OP_NOT) != 0) {
+				return -1;
+			}
 		}
 	}
 
-exit:
-	assert(set->elements_size == elements_size);
-
-	rc = bitmap_iterator_newgroup(pit, &set, 1);
-	free(set);
-
-	return rc;
+	return 0;
 }
 
-static
-int bitmap_index_iterate_contains(struct bitmap_index *index,
-				     struct bitmap_iterator **pit,
-				     void *key, size_t key_size)
+int bitmap_index_iterate_all_set(
+			struct bitmap_index *index,
+			struct bitmap_expr *expr,
+			void *key, size_t key_size)
 {
-	size_t used_bitmaps_count = 1;
+	assert (index != NULL);
+	assert (expr != NULL);
+
+	bitmap_expr_clear(expr);
+
+	if (bitmap_expr_add_group(expr, BITMAP_OP_AND, BITMAP_OP_NULL) != 0) {
+		return -1;
+	}
+
+	if (bitmap_expr_group_add_bitmap(expr, 0,
+		index->bitmaps[0], BITMAP_OP_NULL) != 0) {
+		return -1;
+	}
 
 	size_t pos = 0;
 	while(find_next_set_bit(key, key_size, &pos) == 0) {
 		size_t b = pos + 1;
 		if (index->bitmaps[b] == NULL || b >= index->bitmaps_size) {
-			/* do not have bitmap for this bit */
-			used_bitmaps_count = 0;
+			bitmap_expr_clear(expr);
 			break;
 		}
+		if (bitmap_expr_group_add_bitmap(expr, 0,
+			index->bitmaps[b], BITMAP_OP_NULL) != 0) {
+			return -1;
+		}
 
-		used_bitmaps_count++;
 		pos++;
 	}
 
-	struct bitmap_iterator_group *set;
-	size_t set_size = sizeof(*set) + sizeof(*(set->elements)) *
-			used_bitmaps_count;
-	set = calloc(1, set_size);
-	if (!set) {
-		return -1;
-	}
-
-	set->reduce_op = BITMAP_OP_AND;
-	set->post_op = BITMAP_OP_NULL;
-	set->elements_size = used_bitmaps_count;
-
-	size_t b = 0;
-	if (used_bitmaps_count > 0) {
-		set->elements[b].bitmap = index->bitmaps[0];
-		set->elements[b].pre_op = BITMAP_OP_NULL;
-		b++;
-
-		pos = 0;
-		while(find_next_set_bit(key, key_size, &pos) == 0) {
-			set->elements[b].bitmap = index->bitmaps[pos + 1];
-			set->elements[b].pre_op = BITMAP_OP_NULL;
-			b++;
-
-			pos++;
-		}
-	}
-
-	int rc = bitmap_iterator_newgroup(pit, &set, 1);
-
-	free(set);
-
-	return rc;
+	return 0;
 }
 
-static
-int bitmap_index_iterate_intersects(struct bitmap_index *index,
-				    struct bitmap_iterator **pit,
-				    void *key, size_t key_size)
+int bitmap_index_iterate_any_set(
+			struct bitmap_index *index,
+			struct bitmap_expr *expr,
+			void *key, size_t key_size)
 {
-	int rc = 0;
+	assert (index != NULL);
+	assert (expr != NULL);
 
-	struct bitmap_iterator_group *set1;
-	size_t set_size1 = sizeof(*set1) + sizeof(*(set1->elements)) *
-			(index->bitmaps_size - 1);
-	set1 = calloc(1, set_size1);
-	if (!set1) {
-		rc = -1;
-		goto error_1;
+	bitmap_expr_clear(expr);
+
+	if (bitmap_expr_add_group(expr, BITMAP_OP_OR, BITMAP_OP_NULL) != 0) {
+		return -1;
 	}
-
-	set1->reduce_op = BITMAP_OP_OR;
-	set1->post_op = BITMAP_OP_NULL;
-	set1->elements_size = 0;
 
 	for (size_t pos = 0;
 	     find_next_set_bit(key, key_size, &pos) == 0;
@@ -328,54 +264,23 @@ int bitmap_index_iterate_intersects(struct bitmap_index *index,
 			/* do not have bitmap for this bit */
 			continue;
 		}
-		set1->elements[set1->elements_size].bitmap = index->bitmaps[b];
-		set1->elements[set1->elements_size].pre_op = BITMAP_OP_NULL;
-		set1->elements_size++;
+
+		if (bitmap_expr_group_add_bitmap(expr, 0,
+			index->bitmaps[b], BITMAP_OP_NULL) != 0) {
+			return -1;
+		}
 	}
 
-	struct bitmap_iterator_group *set2;
-	size_t set_size2 = sizeof(*set2) + sizeof(*(set2->elements));
-	set2 = calloc(1, set_size2);
-	if (!set2) {
-		rc = -1;
-		goto error_2;
+	if (bitmap_expr_add_group(expr, BITMAP_OP_AND, BITMAP_OP_NULL) != 0) {
+		return -1;
 	}
 
-	set2->reduce_op = BITMAP_OP_AND;
-	set2->post_op = BITMAP_OP_NULL;
-	set2->elements_size = 1;
-	set2->elements[0].bitmap = index->bitmaps[0];
-	set2->elements[0].pre_op = BITMAP_OP_NULL;
-
-	struct bitmap_iterator_group *groups[] = { set1, set2 };
-
-	rc = bitmap_iterator_newgroup(pit, groups, 2);
-
-	free(set2);
-error_2:
-	free(set1);
-error_1:
-	return rc;
-}
-
-int bitmap_index_iterate(struct bitmap_index *index,
-			struct bitmap_iterator **pit,
-			void *key, size_t key_size,
-			int match_type) {
-
-
-	switch((enum bitmap_index_match_type) match_type) {
-	case BITMAP_INDEX_MATCH_EQUALS:
-		return bitmap_index_iterate_equals(index, pit, key, key_size);
-	case BITMAP_INDEX_MATCH_CONTAINS:
-		return bitmap_index_iterate_contains(index, pit, key, key_size);
-	case BITMAP_INDEX_MATCH_INTERSECTS:
-		return bitmap_index_iterate_intersects(index, pit, key, key_size);
-	default:
-		/* not implemented */
-		errno = ENOTSUP;
-		return -2;
+	if (bitmap_expr_group_add_bitmap(expr, 1,
+		index->bitmaps[0], BITMAP_OP_NULL) != 0) {
+		return -1;
 	}
+
+	return 0;
 }
 
 bool bitmap_index_contains_value(struct bitmap_index *index, size_t value) {

src/box/bitmap/bitmap.h
@@ -31,18 +31,18 @@
  */
 
 /**
+ * @file
  * @brief Module to work with arrays of bits (bitmaps)
+ *
+ * Bitmap is an array of bits where each bit can be set or unset independently.
+ * It supports special compression scheme in order to save memory.
+ * You can use any values in range [0,SIZE_MAX) without worrying about resizing.
+ *
  * @author Roman Tsisyk
  */
 
 #include <util.h>
 
-/**
- * Bitmap object
- * Bitmap is an array of bits where each bit can be set or unset independently.
- * It supports special compression scheme in order to save memory.
- * You can use any values in range [0,SIZE_MAX) without worrying about resizing.
- */
 struct bitmap;
 
 /**
@@ -80,25 +80,6 @@ int bitmap_set(struct bitmap *bitmap, size_t pos, bool val);
  * @return returns the number of bits set to true in this bitmap.
  */
 size_t bitmap_cardinality(struct bitmap *bitmap);
-
-/**
- * @brief BitmapOp is used as parameter to bitmap_iterator_newn function.
- * @see bitmap_iterator_newn
- */
-enum bitmap_unary_op {
-	BITMAP_OP_NULL = 0,
-	/* inverse (apply bitwise NOT) bits in the bitmap  */
-	BITMAP_OP_NOT   = 0x1
-};
-
-enum bitmap_binary_op {
-	BITMAP_OP_AND   = 0x1 << 8,
-	BITMAP_OP_NAND  = 0x2 << 8,
-	BITMAP_OP_OR    = 0x3 << 8,
-	BITMAP_OP_NOR   = 0x4 << 8,
-	BITMAP_OP_XOR   = 0x5 << 8,
-	BITMAP_OP_XNOR  = 0x6 << 8
-};
 
 
 #if defined(DEBUG)

src/box/bitmap/bitmap_p.h
@@ -1,10 +1,6 @@
 #ifndef BITMAP_BITMAP_P_H_INCLUDED
 #define BITMAP_BITMAP_P_H_INCLUDED
 
-/**
- * @file Private header file, please don't use directly.
- */
-
 /*
  * Redistribution and use in source and binary forms, with or
  * without modification, are permitted provided that the following
@@ -32,6 +28,13 @@
  * (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF
  * THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF
  * SUCH DAMAGE.
+ */
+
+/**
+ * @file
+ * @brief Private header file, please don't use directly.
+ * @internal
+ * @author Roman Tsisyk
  */
 
 #include <util.h>

src/box/bitmap/expr_p.h
@@ -1,5 +1,5 @@
-#ifndef TARANTOOL_BOX_INDEX_BITMAP_H_INCLUDED
-#define TARANTOOL_BOX_INDEX_BITMAP_H_INCLUDED
+#ifndef BITMAP_EXPR_P_H_INCLUDED
+#define BITMAP_EXPR_P_H_INCLUDED
 
 /*
  * Redistribution and use in source and binary forms, with or
@@ -31,22 +31,45 @@
  */
 
 /**
- * @brief Module implements adapter (ObjC object) for bitmap index in Tarantool
- * @see struct bitmap
- * @see struct bitmap_index
+ * @file
+ * @brief Private header file, please don't use directly.
  * @author Roman Tsisyk
  */
 
-#include "index.h"
+#include "expr.h"
 
-struct bitmap_index;
-struct bitmap_expr;
+struct bitmap_expr_group {
+	/** operation to combine (reduce) bitmaps during iterations **/
+	enum bitmap_binary_op reduce_op; /* only AND, OR, XOR supported */
+	/** operation to apply after combining */
+	enum bitmap_unary_op post_op; /* not supported yet */
 
-@interface BitmapIndex: Index {
-	@private
-	struct bitmap_index *index;
-	struct bitmap_expr *position_expr;
-}
-@end
+	/** number of elements in the group */
+	size_t elements_size;
+	/** number of allocated elements in the group */
+	size_t elements_capacity;
 
-#endif /* TARANTOOL_BOX_INDEX_BITMAP_H_INCLUDED */
+	/** elements */
+	struct bitmap_expr_group_element {
+		/** operation to apply to source bitmap before reducing */
+		enum bitmap_unary_op pre_op;
+		/** source bitmap */
+		struct bitmap *bitmap;
+	} elements[];
+};
+
+struct bitmap_expr {
+	/** groups */
+	struct bitmap_expr_group **groups;
+	/** number of active groups in the expr */
+	size_t groups_size;
+	/** number of alloacted groups in the expr */
+	size_t groups_capacity;
+
+	bool in_group;
+};
+
+extern const size_t EXPR_DEFAULT_CAPACITY;
+extern const size_t EXPR_GROUP_DEFAULT_CAPACITY;
+
+#endif /* BITMAP_EXPR_P_H_INCLUDED */

src/box/bitmap/index.h
@@ -31,8 +31,9 @@
  */
 
 /**
- * @brief Module implements bitmap index using bitmap objects
- * @see struct bitmap
+ * @file
+ * @brief Bit index.
+ * @see bitmap.h
  * @author Roman Tsisyk
  */
 
@@ -43,69 +44,102 @@
 
 struct bitmap_index;
 
+/**
+ * @brief Creates new index object
+ * @param pindex
+ * @param initial_size initial number of used bitmaps
+ * @return zero on success and non-zero otherwise
+ */
 int bitmap_index_new(struct bitmap_index **pindex, size_t initial_size);
+
+/**
+ * @brief Destroys index object
+ * @param pindex
+ */
 void bitmap_index_free(struct bitmap_index **pindex);
 
 /**
- * @brief Inserts entry to the index
+ * @brief Inserts (key, value) pair into the index.
  * @param index object
- * @param key entry's key
- * @param key_size
- * @param value entry's value
+ * @param key key
+ * @param key_size size of key
+ * @param value value
+ * @return zero on success and non-zero otherwise
  */
 int bitmap_index_insert(struct bitmap_index *index,
 			void *key, size_t key_size,
 			size_t value);
 
-
 /**
- * @brief Removes entry from the index
+ * @brief Removes (key, value) pair from the index.
  * @param index object
- * @param key entry's
- * @param key_size
- * @param value entry's value
+ * @param key key
+ * @param key_size size of key
+ * @param value value
+ * @return zero on success and non-zero otherwise
  */
 int bitmap_index_remove(struct bitmap_index *index,
 			void *key, size_t key_size,
 			size_t value);
 
-
-enum bitmap_index_match_type {
-	/* tuple_set IS SUPERSET of request_set,
-	 * i.e. tuple_set contains all bits from request_set */
-	BITMAP_INDEX_MATCH_CONTAINS = 0,
-
-	/* tuple_set INTERSECTS with request_set */
-	BITMAP_INDEX_MATCH_INTERSECTS = 1,
-
-	/* tuple_set EQUALS request_set */
-	BITMAP_INDEX_MATCH_EQUALS = 2
-};
-
 /**
- * @brief Creates new iterator to iterate over index values
- * Please free pit after using by calling bitmap_iterator_free method.
+ * @brief Equals iteration. Matches all pairs in the index, where
+ * @a key exacttly equals pair.key ((@a key == pair.key).
+ * Initialized @a expr can be used with @a bitmap_iterator_init function.
  * @param index
- * @param pit new bitmap_iterator
- * @param key
- * @param key_size
- * @see struct bitmap_iterator
+ * @param expr expression
+ * @param key key
+ * @param key_size size of key
+ * @return zero on success and non-zero otherwise
+ * @see expr.h
  */
-int bitmap_index_iterate(struct bitmap_index *index,
-			 struct bitmap_iterator **pit,
-			 void *key, size_t key_size,
-			 int match_type);
+int bitmap_index_iterate_equals(
+			struct bitmap_index *index,
+			struct bitmap_expr *expr,
+			void *key, size_t key_size);
 
 /**
- * @brief Checks if value present in the index
+ * @brief All-Bits-Set iteration. Matches all pairs in the index, where
+ * all bits from @a key is set in pair.key ((@a key & pair.key) == @a key).
+ * Initialized @a expr can be used with @a bitmap_iterator_init function.
  * @param index
+ * @param expr expression
+ * @param key key
+ * @param key_size size of key
+ * @return zero on success and non-zero otherwise
+ * @see expr.h
+ */
+int bitmap_index_iterate_all_set(
+			struct bitmap_index *index,
+			struct bitmap_expr *expr,
+			void *key, size_t key_size);
+
+/**
+ * @brief Any-Bits-Set iteration. Matches all pairs in the index, where
+ * at least on bit from @a key is set in pair.key ((@a key & pair.key) != 0).
+ * Initialized @a expr can be used with @a bitmap_iterator_init function.
+ * @param index object
+ * @param expr expression
+ * @param key key
+ * @param key_size size of key
+ * @return zero on success and non-zero otherwise
+ * @see expr.h
+ */
+int bitmap_index_iterate_any_set(
+			struct bitmap_index *index,
+			struct bitmap_expr *expr,
+			void *key, size_t key_size);
+
+/**
+ * @brief Checks if a pairs with @a value exists in the index
+ * @param index object
  * @param value
- * @return true if the index contains entry with this value
+ * @return true if the index contains pair with the @a value
  */
 bool bitmap_index_contains_value(struct bitmap_index *index, size_t value);
 
 /**
- * @brief Returns number of entries in this index.
+ * @brief Returns number of pairs in the index.
  * @param index object
  * @return number of pairs in this index
  */

commit 27d4ad8a9065ebc8e6a980f0251254c613c89460

Fix a regression introduced by 6965dc8a4a78bd4927d5fe4f9d896077ae5d2d32

src/box/bitmap/expr.c
@@ -223,6 +223,15 @@ bitmap_expr_group_add_bitmap(struct bitmap_expr *expr,
 	return 0;
 }
 
+void
+bitmap_expr_group_clear(struct bitmap_expr *expr,
+			size_t group_id)
+{
+	assert(expr != NULL);
+	assert(expr->groups_size > group_id);
+	expr->groups[group_id]->elements_size = 0;
+}
+
 size_t
 bitmap_expr_group_size(struct bitmap_expr *expr,
 		       size_t group_id)

src/box/bitmap/expr.h
@@ -147,6 +147,15 @@ bitmap_expr_group_add_bitmap(struct bitmap_expr *expr,
 			     enum bitmap_unary_op pre_op);
 
 /**
+ * @brief Clear group with identifier = @a group_id
+ * @param expr object
+ * @param group_id  group identifier
+ */
+void
+bitmap_expr_group_clear(struct bitmap_expr *expr,
+			size_t group_id);
+
+/**
  * @brief Returns number of bitmaps in the @a group
  * @param expr object
  * @param group_id group identifier

src/box/bitmap/index.c
@@ -182,7 +182,7 @@ int bitmap_index_iterate_equals(
 		size_t b = pos + 1;
 		if (test_bit(key, pos)) {
 			if (index->bitmaps[b] == NULL) {
-				bitmap_expr_clear(expr);
+				bitmap_expr_group_clear(expr, 0);
 				break;
 			}
 
@@ -200,6 +200,11 @@ int bitmap_index_iterate_equals(
 				return -1;
 			}
 		}
+	}
+
+	if (bitmap_expr_group_add_bitmap(expr, 0,
+		index->bitmaps[0], BITMAP_OP_NULL) != 0) {
+		return -1;
 	}
 
 	return 0;